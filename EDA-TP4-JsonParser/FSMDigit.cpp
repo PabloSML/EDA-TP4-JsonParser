0a50221e6c79755a67acd12c658c0cbe24364eac
User <User@DESKTOP-AMR1CVM>
2019-04-07 22:03:12 -0300

    x

--- EDA-TP4-JsonParser/FSMDigit.cpp
+++ EDA-TP4-JsonParser/FSMDigit.cpp
@@ -6,41 +6,50 @@
 using namespace std;
 
 void FSMDigit::cycle(string& s) {
-
-	const FMSCell FSMTable[STATES][EVENTS] = { {DIGIT, continueToDo}, {DIGIT, continueToDo}, {DIGIT, continueToDo}, {COMA, continueToDo}, {EXPONENT, continueToDo}, {EXPONENT, continueToDo}, {DIGIT, continueToDo},
-											{ZERONOT, continueToDo},  {DIGIT, continueToDo}, {DIGIT, continueToDo}, {COMA, continueToDo}, {EXPONENT, continueToDo}, {EXPONENT, continueToDo}, {ERROR, error},
-											{ERROR, error},           {ERROR, error},        {COMA, continueToDo},  {ERROR, error},       {ERROR, error},           {ERROR, error},			  {COMA, continueToDo},
-											{NEGATIVE, continueToDo}, {ERROR, error},        {ERROR, error},        {ERROR, error},       {EXPONENT, continueToDo}, {ERROR, error},           {ERROR, error},
-											{ERROR, error},           {ERROR, error},        {EXPOI, continueToDo}, {EXPOI, continueToDo},{ERROR, error},			{ERROR, error},           {EXPOI, continueToDo},
-											{ERROR, error},           {ERROR, error},        {ERROR, error},        {ERROR, error},       {EXPONENT, continueToDo}, {ERROR, error},           {ERROR, error},
-											{ERROR, error},           {ERROR, error},        {ERROR, error},        {ERROR, error},       {ERROR, error},			{ERROR, error},           {ERROR, error} }
+	 
+	const FMSCell FSMTable[STATES][EVENTS] = { {   {DIGIT, continueToDo},    {DIGIT, continueToDo}, {DIGIT, continueToDo}, {COMA, continueToDo}, {states::EXPONENT, continueToDo},  {EXPONENT, continueToDo}, {DIGIT, continueToDo} , {ERROR, error}  },
+												{  {ZERONOT, continueToDo},  {DIGIT, continueToDo}, {DIGIT, continueToDo}, {COMA, continueToDo}, {states::EXPONENT, continueToDo},  {EXPONENT, continueToDo}, {ERROR, error} , {ERROR, error}  },
+												{  {ERROR, error},           {ERROR, error},        {COMA, continueToDo},  {ERROR, error},       {ERROR, error},            {ERROR, error},			  {COMA, continueToDo}, {ERROR, error} },
+												{  {NEGATIVE, continueToDo}, {ERROR, error},        {ERROR, error},        {ERROR, error},       {states::EXPONENT, continueToDo},  {ERROR, error},           {ERROR, error}, {ERROR, error}   },
+												{  {ERROR, error},           {ERROR, error},        {EXPOI, continueToDo}, {EXPOI, continueToDo},{ERROR, error},			{ERROR, error},           {EXPOI, continueToDo}, {ERROR, error} },
+												{  {ERROR, error},           {ERROR, error},        {ERROR, error},        {ERROR, error},       {EXPONENT, continueToDo},  {ERROR, error},           {ERROR, error}, {ERROR, error}   },
+												{  {ERROR, error},           {ERROR, error},        {ERROR, error},        {ERROR, error},       {ERROR, error},			{ERROR, error},           {ERROR, error},  {ERROR, error}, } };
 	bool ok = true; //corta antes si el programa dio error
 	states state = INITIAL; 
 	events event_;
-	JSONError error_t(true,"");
+	JSONError error_t(true, '');
 	for (int i = 0; i < s.length() && ok; i++) {
-		if (isdigit(s[i]) && s[i] != "0") {
-			event_ = DIGIT;
+		if (isdigit(s[i]) && s[i] != 0) {
+			event_ = events::DIGITS;
 		}
-		else if (s[i] == "0") {
-			event_ = ZERO;
+		else if (s[i] == '0') {
+			event_ = events::ZERO;
 		}
-		else if (s[i] == ".") {
-			event_ = COMA;
+		else if (s[i] == '.') {
+			event_ = events::COMA;
 		}
-		else if (s[i] == "-") {
-			event_ = NEGATIVE;
+		else if (s[i] == '-') {
+			event_ = events::NEGATIVE;
 		}
-		else if (s[i] == "E" || s[i] == "e") {
-			event_ == EXPONENT;
+		else if (s[i] == 'E' || s[i] == 'e') {
+			event_ == events::EXPONENT;
 		}
-		else if (s[i] == "+") {
-			event_ = PLUS;
+		else if (s[i] == '+') {
+			event_ = events::PLUS;
 		}
 		else{
-			event_ = ELSE;
+			event_ = events::ELSE;
 		}
-		ok = FSMTable[state][event_].action(error_t);
+		ok = FSMTable[state][event_].action(&error_t);
 		state = FSMTable[state][event_].nextState;
 	}
+}
+
+;bool continueToDo(void* UserData){
+	return true;
+}
+
+bool error(void* UserData) {
+	((*JSONError)UserData)->setErrorString(ERROR_NUM);
+	return false;
 }
